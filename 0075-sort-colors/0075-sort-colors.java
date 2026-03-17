5f179f220d5b829d7ba81d396d524edb963b12b9
Dhurgashree I <dhurgashreei.23it@kongu.edu>
2026-03-17 19:29:26 +0530

    Time: 3 ms (15.22%), Space: 43 MB (99.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,13 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
         for(int i=0;i<nums.length;i++){
+            boolean swap=false;
             for(int j=1;j<nums.length-i;j++){
                 if(nums[j]<nums[j-1]){
                     int temp=nums[j];
                     nums[j]=nums[j-1];
                     nums[j-1]=temp;
+                    swap=true;
                 }
             }
+            if(!swap) return;
         }
     }
 }
